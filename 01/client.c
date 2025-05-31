10d1e69e69a93d863f47d579053f2fef4102f020
miroslavkramar <miroslav.kramar@tul.cz>
2025-05-31 18:33:35 +0200

    Added unbuffered progress reporting

--- 01/client.c
+++ 01/client.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
 #define IO_BUF_LEN 255
+
+__attribute__((format(printf, 1, 2))) void report_msg(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+}
 
 int main()
 {
@@ -14,17 +22,17 @@ int main()
     const char * err_msg = NULL;
     
     // create socket
-    printf("Creating socket... ");
+    report_msg("Creating socket... ");
     int sockfd = -1;
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
         err_msg = "Creating socket failed";
         goto ERR_COMM;
     }
-    printf("done\n");
+    report_msg("done\n");
     
     // create address data
-    printf("Initializing address data... ");
+    report_msg("Initializing address data... ");
     struct sockaddr_in addr = {0};
     addr.sin_family = AF_INET;
     addr.sin_port = htons(12345);
@@ -33,35 +41,35 @@ int main()
         fprintf(stderr, "Invalid address provided in address initialization!\n");
         goto ERR_COMM;
     }
-    printf("done\n");
+    report_msg("done\n");
 
     // connect socket to address
-    printf("Connecting socket to address... ");
+    report_msg("Connecting socket to address... ");
     status = connect(sockfd, (struct sockaddr *) &addr, sizeof(addr));
     if (status < 0) {
         err_msg = "Socket connection error";
         goto ERR_COMM;
     }
-    printf("done\n");
+    report_msg("done\n");
 
     // send message
-    printf("Sending data... ");
+    report_msg("Sending data... ");
     snprintf(io_txt_buf, IO_BUF_LEN, "Hello from the Client!\n");
     ssize_t bytes_sent = send(sockfd, io_txt_buf, strlen(io_txt_buf), 0);
     if (bytes_sent < 0) {
         err_msg = "Sending data failed";
         goto ERR_COMM;
     }
-    printf("done\n");
+    report_msg("done\n");
 
     // receive message
-    printf("Receiveing data... ");
+    report_msg("Receiveing data... ");
     ssize_t bytes_received = recv(sockfd, io_txt_buf, IO_BUF_LEN, 0);
     if (bytes_received < 0) {
         err_msg = "Receiving data failed";
         goto ERR_COMM;
     }
-    printf("done\n");
+    report_msg("done\n");
 
     // process message
     if (bytes_received >= 0 && bytes_received < IO_BUF_LEN) {
@@ -78,7 +86,12 @@ int main()
 
     // error handling
     ERR_COMM:
-    if (err_msg) perror(err_msg);
+    fflush(stdout);
+    fflush(stderr);
+    if (err_msg) {
+        putchar('\n');
+        perror(err_msg);
+    }
     if (sockfd >= 0) close(sockfd);
     return EXIT_FAILURE;
 }
